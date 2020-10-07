e80d77453a94b5ee518f5c5d4f33fe2978779768
akqbi168 <akqbi168@gmail.com>
2020-10-07 09:11:50 +0900

    update db integer

--- db/migrate/20201006040714_create_news.rb
+++ db/migrate/20201006040714_create_news.rb
@@ -2,9 +2,9 @@ class CreateNews < ActiveRecord::Migration[5.2]
   def change
     create_table :news do |t|
 
-      t.integer :year, limit: 2199
-      t.integer :month, limit: 12
-      t.integer :date, limit: 31
+      t.integer :year
+      t.integer :month
+      t.integer :date
       t.string :title
       t.boolean :published
       t.timestamps
