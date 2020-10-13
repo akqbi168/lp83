11ca2fa6b56e81745da6da5302215cfff4697beb
akqbi168 <akqbi168@gmail.com>
2020-10-13 09:57:13 +0900

    add refile key config

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,6 @@
 #     https: false
 #   )
 # end
+
+# added for refile
+Refile.secret_key = '8f05726f446a0ffe63fda9a6b14b8d3f8abc40f1d5aefe4a9a215e8f4e606e13ff8bf3883410d5e784130f1764b90d813d1b79ff05106ad3abf969787380a94a'
