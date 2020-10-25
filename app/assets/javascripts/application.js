2c11a0c19c5acf1b78866135b2abf1b1a0cf58fe
akqbi168 <akqbi168@gmail.com>
2020-10-26 08:09:41 +0900

    responsive, background

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,6 +18,43 @@
 
 
 
+// スムーススクロール
+$(document).ready(function(){
+  //URLのハッシュ値を取得
+  var urlHash = location.hash;
+  //ハッシュ値があればページ内スクロール
+  if(urlHash) {
+    //スクロールを0に戻す
+    $('body,html').stop().scrollTop(0);
+    setTimeout(function () {
+      //ロード時の処理を待ち、時間差でスクロール実行
+      scrollToAnker(urlHash) ;
+    }, 500);
+  }
+
+  //通常のクリック時
+  $('a[href^="#"]').click(function() {
+    //ページ内リンク先を取得
+    var href= $(this).attr("href");
+    //リンク先が#か空だったらhtmlに
+    var hash = href == "#" || href == "" ? 'html' : href;
+    //スクロール実行
+    scrollToAnker(hash);
+    //リンク無効化
+    return false;
+  });
+
+  // 関数：スムーススクロール
+  // 指定したアンカー(#ID)へアニメーションでスクロール
+  function scrollToAnker(hash) {
+    var target = $(hash);
+    var position = target.offset().top;
+    $('body,html').stop().animate({scrollTop:position}, 1000);
+  }
+})
+
+
+
   //画面高さ取得
   $(window).load(function() {
     h = $(window).height();
@@ -34,4 +71,3 @@
 
 // jQueryを使うなら
 // $('#box').css('height',$(window).height());
-
